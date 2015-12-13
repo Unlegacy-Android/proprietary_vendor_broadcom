05c7ebecbdc7e18f1a7a9c8feff23e2ed63af068
Ziyan <jaraidaniel@gmail.com>
2015-12-13 22:21:47 +0100

    Move proprietary blobs to /vendor

--- grouper/broadcom-vendor-blobs.mk
+++ grouper/broadcom-vendor-blobs.mk
@@ -1,7 +1,7 @@
 VENDOR_FOLDER := vendor/broadcom/grouper
 
 PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/bin/glgps:system/bin/glgps \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
 	$(VENDOR_FOLDER)/proprietary/etc/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
-	$(VENDOR_FOLDER)/proprietary/lib/hw/gps.tegra3.so:system/lib/hw/gps.tegra3.so
+	$(VENDOR_FOLDER)/proprietary/vendor/bin/glgps:system/vendor/bin/glgps \
+	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/gps.tegra3.so:system/vendor/lib/hw/gps.tegra3.so

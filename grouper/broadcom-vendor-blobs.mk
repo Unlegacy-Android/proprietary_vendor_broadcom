5ea56cb0aedfb75e2b06d7c8316b7fee0530ebc7
Ziyan <jaraidaniel@gmail.com>
2016-08-30 22:05:25 +0200

    grouper: switch to gpsd from manta (1/2)
    Android 5.1.1 (LMY49J) release, plus:
    - link against libgpsd-compat
    - update SensorService::getSensorList call for N userspace compatibility

--- grouper/broadcom-vendor-blobs.mk
+++ grouper/broadcom-vendor-blobs.mk
@@ -2,5 +2,5 @@ VENDOR_FOLDER := vendor/broadcom/grouper
 
 PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
-	$(VENDOR_FOLDER)/proprietary/vendor/bin/glgps:system/vendor/bin/glgps \
+	$(VENDOR_FOLDER)/proprietary/vendor/bin/gpsd:system/vendor/bin/gpsd \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/gps.tegra3.so:system/vendor/lib/hw/gps.tegra3.so

44d698d8bf9a2aec81905ca48b8e824a0398e33a
Ziyan <jaraidaniel@gmail.com>
2016-08-30 20:45:13 +0200

    grouper: move gps.xml to the device tree (1/2)

--- grouper/broadcom-vendor-blobs.mk
+++ grouper/broadcom-vendor-blobs.mk
@@ -2,6 +2,5 @@ VENDOR_FOLDER := vendor/broadcom/grouper
 
 PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
-	$(VENDOR_FOLDER)/proprietary/etc/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
 	$(VENDOR_FOLDER)/proprietary/vendor/bin/glgps:system/vendor/bin/glgps \
 	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/gps.tegra3.so:system/vendor/lib/hw/gps.tegra3.so
